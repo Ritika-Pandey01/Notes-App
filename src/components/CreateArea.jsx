be2cdf741bcfb5cc864f8dc1de75f527b2e7eb51
Ritika <ritika.pandey2020@vitbhopal.ac.in>
2022-07-28 19:25:51 +0530

    Final commit  of notes app

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+
+function CreateArea(props) {
+
+  const [note,setNote]=useState({
+    title:"",
+    content:""
+  });
+  function handleChange(event){
+    const {name,value}=event.target;
+    setNote(preNote=>{
+      return {
+        ...preNote,[name]:value
+      }
+    });
+  }
+
+  function submitNote(event){
+    props.onAdd(note);
+    setNote({title:"",
+    content:""});
+    event.preventDefault();
+
+  }
+  return (
+    <div>
+      <form>
+        <input onChange={handleChange} name="title" value={note.title} placeholder="Enter title" />
+        <textarea onChange={handleChange}  name="content" value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
